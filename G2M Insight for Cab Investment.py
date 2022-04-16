8befc1a3b2dcd65902fa04ee38db63e9d7873438
Febraizel <Seancchen98@gmail.com>
2022-04-16 00:14:39 -0400

    Enter

---
+++ G2M Insight for Cab Investment.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+cab_data = pd.read_csv("Cab_Data.csv")
+city = pd.read_csv("City.csv")
+CustID = pd.read_csv("Customer_ID.csv")
+TransID = pd.read_csv("Transaction_ID.csv")
+#print(cab_data.columns)
+#cabdshape = cab_data.shape
+#print(cabdshape)
+#cityshape = city.shape
+#print(cityshape)
+#Custshape = CustID.shape
+#print(Custshape)
+#Transshape = TransID.shape
+#print(Transshape)
